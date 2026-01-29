eb1a81f835c5227ae1ba20acca3144e78f8bc214
guillenfernanda45-droid <guillenfernanda45@gmail.com>
2026-01-29 19:53:08 +0000

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-pip
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+st.title("App de streamlit")
+st.header("Este es un encabezado")
+st.subheader("Este es un subencabezado")
+st.write("Este es un parrafo")
+st.write("Este es un segundo parrafo")
+st.write("Este es un tercer parrafo")
+"_________________________________"
+
+df = pd.dataframe(np.random.randn(10, 2), colums=['precios', 'ventas'])
+st.dataframe(df)
