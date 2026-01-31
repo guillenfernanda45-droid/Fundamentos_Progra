daed5541c85f5eca8536fe8956f943f859718d04
guillenfernanda45-droid <guillenfernanda45@gmail.com>
2026-01-31 23:22:32 +0000

    Cambios

--- app.py
+++ app.py
@@ -8,7 +8,24 @@ st.subheader("Este es un subencabezado")
 st.write("Este es un parrafo")
 st.write("Este es un segundo parrafo")
 st.write("Este es un tercer parrafo")
+
 "_________________________________"
 
-df = pd.dataframe(np.random.randn(10, 2), colums=['precios', 'ventas'])
+df = pd.DataFrame(np.random.randn(10, 2), columns=['precios', 'ventas'])
+st.line_chart(df, x_label="Posición", y_label="Valor", color= ["#B439B0","#C16B20"])
 st.dataframe(df)
+
+"__________________________________"
+
+st.markdown("""
+# hola  
+```
+import streamlit as st 
+import pandas as pd
+import numpy as np
+```
+""")
+
+"""
+## hola
+"""
